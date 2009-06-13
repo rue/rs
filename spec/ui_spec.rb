192e7a340a21196560f15de0b74916b4e5d3539f
Eero Saynatkari <rs@projects.kittensoft.org>
2009-06-13 09:35:49 +0300

    Basic specs for Readline-based console UI.

---
+++ spec/ui_spec.rb
@@ -0,0 +1,141 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+# Testing
+require "spec_helper"
+
+# Project
+require "rs/ui"
+
+
+describe "UI creation" do
+
+  it "yields the UI instance to the given block" do
+    RS::UI.new {|ui| ui.should be_kind_of(RS::UI) }
+  end
+
+  it "provides some kind of a default prompt String" do
+    mock(Readline).readline(satisfy {|arg| arg.kind_of? String }, true).times(1) { nil }
+
+    RS::UI.new {|ui| ui.run }
+  end
+
+  it "allows prompt to be assigned to" do
+    lambda {
+      RS::UI.new {|ui| ui.prompt = lambda { "hi>" } }
+    }.should_not raise_error
+  end
+
+  it "accepts a ^C handler in #on_SIGINT" do
+    lambda {
+      RS::UI.new {|ui| ui.on_SIGINT { :hi } }
+    }.should_not raise_error
+  end
+
+  it "accepts a linewise input handler in #on_input" do
+    lambda {
+      RS::UI.new {|ui| ui.on_input {|i| i.reverse } }
+    }.should_not raise_error
+  end
+
+end
+
+
+describe "UI loop/processing" do
+
+  it "terminates when ^D, EOF, received." do
+    mock(Readline).readline(anything, anything).times(1) { nil }
+
+    RS::UI.new {|ui|
+      ui.run
+    }
+  end
+
+  it "calls the current prompt and uses its value going out for each loop" do
+    str = "rs_ui_spec_prompt #{$$}> "
+    mock(prompt = Object.new).call.times(2) { str }
+
+    inputs = ["hi\n", "ho\n", nil]
+    prompts = [//, str, str]
+
+    mock(Readline).readline(anything, anything).times(3) {|pmpt, i|
+      prompts.shift.should === pmpt
+      inputs.shift
+    }
+
+    RS::UI.new {|ui|
+      ui.on_input {|input|
+        ui.prompt = prompt
+      }
+
+      ui.run
+    }
+  end
+
+end
+
+
+describe "UI event handling" do
+
+  it "calls block given to #on_SIGINT on ^C" do
+    inputs = [lambda { raise Interrupt }, lambda { nil }]
+
+    stub(Readline).readline(anything, anything).times(2) { inputs.shift.call }
+
+    called = false
+
+    RS::UI.new {|ui|
+      ui.on_SIGINT { called = true }
+      ui.run
+    }
+
+    called.should == true
+  end
+
+  it "continues processing after invoking #on_SIGINT" do
+    continued = false
+
+    inputs = [lambda { raise Interrupt }, lambda { continued = true; nil } ]
+
+    stub(Readline).readline(anything, anything).times(2) { inputs.shift.call }
+
+    RS::UI.new {|ui|
+      ui.on_SIGINT { :yay }
+      ui.run
+    }
+
+    continued.should == true
+  end
+
+  it "invokes #on_input block for each line of input until terminated by ^D" do
+    inputs = ["hi\n", "ha\n", "hoo bee\n", nil, "blee\n"]
+    expected = inputs.map {|i| i.chomp if i }
+
+    stub(Readline).readline(anything, anything).times(4) { inputs.shift }
+
+    RS::UI.new {|ui|
+      ui.on_input {|input|
+        input.should == expected.shift
+      }
+
+      ui.run
+    }
+
+    expected.should == [nil, "blee"]
+  end
+
+end
+
+
+describe "UI output" do
+
+  it "calls #to_s on its argument and writes it to output with a newline when using #puts" do
+    lambda {
+      RS::UI.new {|ui|
+        ui.puts "Hiya"
+        ui.puts "Really"
+      }
+    }.should output("Hiya\nReally\n")
+  end
+
+end
+
